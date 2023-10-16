0b20fca4e5dd3e8fda0f6c6e161cf091b18da44e
chashao <chashao246972@gmail.com>
2023-10-16 16:25:00 +0800

    工程初始化

---
+++ src/main/java/group/ordersystem/OrderSystemApplication.java
@@ -0,0 +1,13 @@
+package group.ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/ordersystem/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package group.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
