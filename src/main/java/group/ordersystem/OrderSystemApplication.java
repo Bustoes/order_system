a862277d0414d84a82003a231ba19e3325154fd0
chashao <chashao246972@gmail.com>
2023-10-17 12:31:10 +0800

    FIX:pom.xml中maven版本为8 FIX:springboot、mybatis版本、Druid数据源

--- src/main/java/group/ordersystem/OrderSystemApplication.java
+++ src/main/java/group/ordersystem/OrderSystemApplication.java
@@ -2,12 +2,14 @@ package group.ordersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class OrderSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderSystemApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(OrderSystemApplication.class, args);
+
     }
 
 }
