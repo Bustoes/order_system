5ce0c3b6a81cd078d6d6def0352332b1655e4e93
chashao <chashao246972@gmail.com>
2023-11-15 23:36:55 +0800

    ADD:送餐员和餐厅职工

---
+++ src/main/java/group/ordersystem/controller/DeliverController.java
@@ -0,0 +1,9 @@
+package group.ordersystem.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/deliver")
+public class DeliverController {
+}
