ab393770f9e7bf14bde961816a077d12567c5564
ruo371 <2532883990@qq.com>
2023-11-29 21:20:42 +0800

    FIX:新增：登陆时需要输入：identity验证；并将登录注册函数封装到commonservice（lmpl）类接口里 ADD:  /delivery/order 获取系统全部订单 ADD:  /delivery/myorder 仅获取delivery自己的订单 ADD:  /delivery/order/{order_id} 接单 ADD:  /delivery/delete/{order_id} 在取餐前可执行：将已接单订单取消 ADD:  /delivery/take/{order_id} 取餐，订单状态：2->3 ADD:  /delivery/delivery/{order_id} 确认送达，订单状态：3->4

--- src/main/java/group/ordersystem/controller/DeliveryController.java
+++ src/main/java/group/ordersystem/controller/DeliveryController.java
@@ -1,9 +1,83 @@
 package group.ordersystem.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import group.ordersystem.pojo.res.GetOrdersRes;
+import group.ordersystem.service.DeliveryService;
+import group.ordersystem.util.response.UniversalResponse;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/delivery")
 public class DeliveryController {
+
+    @Resource
+    private DeliveryService deliveryService;
+    /**
+     * 查询系统所有订单
+     *
+     * @return 返回系统订单
+     * Author ruo371
+     */
+    @GetMapping("/order")
+    @ResponseBody
+    public UniversalResponse<List<GetOrdersRes>> getOrders1() {
+        return deliveryService.getOrder();
+    }
+    /**
+     * 查询自己已订订单
+     *
+     * @return 返回自己订单
+     * Author ruo371
+     */
+    @GetMapping("/myorder")
+    @ResponseBody
+    public UniversalResponse<List<GetOrdersRes>> getOrders2() {
+        return deliveryService.getmyOrder();
+    }
+    /**
+     * 接单
+     *
+     * @return 无
+     * Author ruo371
+     */
+    @PostMapping("/order/{order_id}")
+    @ResponseBody
+    public UniversalResponse<?> postOrder1(@PathVariable Integer order_id) {
+        return deliveryService.take_order(order_id);
+    }
+    /**
+     * 取餐
+     *
+     * @return 无
+     * Author ruo371
+     */
+    @PostMapping("/take/{order_id}")
+    @ResponseBody
+    public UniversalResponse<?> postOrder2(@PathVariable Integer order_id) {
+        return deliveryService.take_meal(order_id);
+    }
+    /**
+     * 订单已送达
+     *
+     * @return 无
+     * Author ruo371
+     */
+    @PostMapping("/delivery/{order_id}")
+    @ResponseBody
+    public UniversalResponse<?> postOrder3(@PathVariable Integer order_id) {
+        return deliveryService.delivery_meal(order_id);
+    }
+    /**
+     * 在去餐前，取消订单
+     *
+     * @return 无
+     * Author ruo371
+     */
+    @PostMapping("/delete/{order_id}")
+    @ResponseBody
+    public UniversalResponse<?> postOrder4(@PathVariable Integer order_id) {
+        return deliveryService.delete_meal(order_id);
+    }
 }
